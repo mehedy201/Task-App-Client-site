239fad9d025e72082cd076f54133a88dbbea2668
mehedy201 <mehedy643@gmail.com>
2022-06-30 23:14:01 +0600

    created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mehedi</h1>
-      
+    <div className='xl:max-w-[1140px] lg:max-w-[90%] md:max-w-[90%] sm:max-w-[90%] w-[95%] mx-auto'>
+      <Header></Header>
     </div>
   );
 }
